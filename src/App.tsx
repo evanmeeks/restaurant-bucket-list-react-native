83668401658053c8eaa7ecb231525f4f553ba3dd
evan.meeks@gmail.com <Gassw0rds9>
2019-03-21 11:18:58 -0500

    add store redux and migrate the reducers the js project https://github.com/evanmeeks/restaurant-list-native

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
-export default class App extends React.Component {
-  public render() {
+const store = configureStore();
+
+export default class App extends Component {
+  render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.tsx to develop working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to develop working on your app!</Text>
+        </View>
+      </Provider>
     );
   }
 }
