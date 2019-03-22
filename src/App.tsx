24a3751cc24f4db999c6e43f3eff094d2fc4f2c2
evan.meeks@gmail.com <Gassw0rds9>
2019-03-22 13:43:49 -0500

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import Router from './router';
-import Header from './components/Header';
 
 const store = configureStore();
 
@@ -16,12 +14,3 @@ export default class App extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
